0f66edb4dc2f2e4bb22b8cfc7f991f68d0087d9d
jaimin25 <jaiminchovatia25123@gmail.com>
2023-12-03 20:55:24 +0530

    Add upload server image

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -24,6 +24,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useEffect, useState } from "react";
+import FileUpload from "@/components/file-upload";
 
 const formSchema = z.object({
     name: z.string().min(1, { message: "Server name is required" }),
@@ -71,7 +72,21 @@ const InitialModal = () => {
                         className="space-y-8">
                         <div className="space-y-8 px-6">
                             <div className="flex items-center justify-center text-center">
-                                TODO: Image Upload
+                                <FormField
+                                    control={form.control}
+                                    name="imageUrl"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormControl>
+                                                <FileUpload
+                                                    endpoint="serverImage"
+                                                    value={field.value}
+                                                    onChange={field.onChange}
+                                                />
+                                            </FormControl>
+                                        </FormItem>
+                                    )}
+                                />
                             </div>
                             <FormField
                                 control={form.control}
